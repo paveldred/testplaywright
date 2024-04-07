278ec95bb7abb2bafe47fea1f6f62833b3d309bd
Pavel Smolkov <smolcov@bk.ru>
2024-04-07 23:32:06 +0700

    Initial commit

---
+++ tests/example.test.js
@@ -0,0 +1,26 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Проверка заголовка', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('Отображается ссылка Get started', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+
+test('Мой первый тест', () => {
+  
+  //console output 
+  console.log("Hello, Inzhenerka.Tech!");
+});
---
+++ tests/lesson2_click.spec.js
@@ -0,0 +1,43 @@
+const { test, expect } = require('@playwright/test');
+
+test('click test', async ({ page }) => {
+    await page.goto('http://uitestingplayground.com/click');
+    
+    // >>>>> дополнить код здесь
+    await page.locator("#badButton").click();
+    // <<<<<
+    
+    await expect(await page.locator("#badButton").getAttribute("class")).toMatch(/btn-success/);
+  });
+
+  test('checkbox and radio', async({page}) => {
+    await page.goto('https://jqueryui.com/resources/demos/checkboxradio/default.html');
+      
+    // >>>>> дополнить код здесь
+    await page.locator("[for=radio-1]").click();
+    await page.locator("[for=checkbox-4]").click();
+    await page.locator("[for=checkbox-nested-3]").click({});
+    await page.locator("[for=checkbox-nested-4]").click();
+    // <<<<<
+      
+    await expect(page.locator("[for=radio-1]")).toHaveClass(/ui-checkboxradio-checked/);
+    await expect(page.locator("[for=checkbox-4]")).toHaveClass(/ui-checkboxradio-checked/);
+    await expect(page.locator("[for=checkbox-nested-3]")).toHaveClass(/ui-checkboxradio-checked/);
+    await expect(page.locator("[for=checkbox-nested-4]")).toHaveClass(/ui-checkboxradio-checked/);
+  });
+
+  test('log in positive', async({page}) => {
+    await page.goto('http://uitestingplayground.com/sampleapp');
+      
+    // >>>>> дополнить код здесь
+    const login = "User1";
+
+    await page.locator("[name=UserName]").fill(login);
+    await page.locator("[name=Password]").fill('pwd');
+    await page.locator("#login").click();
+
+    // <<<<<
+    //await expect(page.locator("#loginstatus").getAttribute("class")).toMatch(/text-success/);
+    await expect(page.locator("#loginstatus")).toHaveText(`Welcome, ${login}!`);
+
+  });
