a7de437f8bb80cc01af543cc487a3334f8dea0fb
Pavel Smolkov <smolcov@bk.ru>
2024-04-28 14:30:54 +0700

    lesson 3

--- tests/lesson2_screenshot.spec.js
+++ tests/lesson2_screenshot.spec.js
@@ -1,9 +1,18 @@
 const { test, expect } = require('@playwright/test');
+const fs = require('fs');
+
 
 test('screenshot', async ({ page }) => {
   await page.goto('http://uitestingplayground.com/sampleapp');
   
-  await page.locator("body").screenshot({path: "body.png"})
-  await page.locator(".container").nth(0).screenshot({path: "container.png"})
+  await page.locator("body").screenshot({path: "body.png"});
+  await page.locator(".container").nth(0).screenshot({path: "container.png"});
+
+  await expect(page.locator("#updatingButton")).toHaveText("AbcAbcAbc")
+  await expect(page.locator("#updatingButton")).toHaveText("AbcAbcAbc")
+
+  expect(fs.existsSync('body.png')).toBe(true)
+  expect(fs.existsSync('container.png')).toBe(true)
+
 
 });
